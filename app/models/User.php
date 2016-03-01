096682d7201b2b264a1141fd40a220947e6b3952
Matthew Catellier <mattcatellier@gmail.coml>
2016-02-29 16:28:18 -0800

    added validation logic into the Use model

--- app/models/User.php
+++ app/models/User.php
@@ -9,20 +9,28 @@ class User extends Eloquent implements UserInterface, RemindableInterface {
 
 	use UserTrait, RemindableTrait;
 
-	/**
-	 * The database table used by the model.
-	 *
-	 * @var string
-	 */
+	// link to a table, declare which fields you can edit
 	protected $table = 'users';
-
 	protected $fillable = ['username', 'password'];
-
-	/**
-	 * The attributes excluded from the model's JSON form.
-	 *
-	 * @var array
-	 */
 	protected $hidden = array('password', 'remember_token');
+
+	// to store error messages
+	public $messages;
+
+	// define all the rules for the form
+	public static $rules = [
+		'username' => 'required|email', // email is required and is actually an email
+		'password' => 'required|alphaNum|min:3' // numbers and letter, more than 3 letters
+	];
+
+	// create method to check if the form is valid
+	public function isValid() {
+		$v = Validator::make($this->attributes, static::$rules);
+		if($v->passes()) {
+			return true;
+		}
+		$this->messages = $v->messages();
+		return false;
+	}
 
 }
